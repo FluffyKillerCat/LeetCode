b624d710799c8bd6f4f021f7cefe13e658975b62
Joud Hajal <joudhajal@Mac.fritz.box>
2025-01-29 11:36:07 +0100

    Solved Letter Combinations of a Phone Number

---
+++ Letter Combinations of a Phone Number.py
@@ -0,0 +1,26 @@
+def letter_combinations(digits):
+    digit_to_char = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz'
+    }
+
+    def backtrack(combination, next_digits):
+        if len(next_digits) == 0:
+            output.append(combination)
+        else:
+            for letter in digit_to_char[next_digits[0]]:
+                backtrack(combination + letter, next_digits[1:])
+
+    output = []
+    if digits:
+        backtrack("", digits)
+    return output
+
+# Example usage
+print(letter_combinations("23"))  # Output: ['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf']
